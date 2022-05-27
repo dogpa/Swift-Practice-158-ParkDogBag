9307bca96256a220edadbf71c7f2419ee35727ae
Dogpa <dogpa0130@gmail.com>
2022-05-27 12:52:02 +0800

    Initial Commit

---
+++ ParkDogBag/ParkDogBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkDogBagApp.swift
+//  ParkDogBag
+//
+//  Created by Dogpa's MBAir M1 on 2022/5/27.
+//
+
+import SwiftUI
+
+@main
+struct ParkDogBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
